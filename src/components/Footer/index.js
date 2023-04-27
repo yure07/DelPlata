c1c886560460c7eed5422f452cfe41bca8daf809
Yure Rafael <112043267+yure07@users.noreply.github.com>
2023-04-27 16:38:44 -0300

    footer-update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import './footer.css'
 import logo from '../../assets/logo-com-fundo.jpeg'
-import cartoes from '../../assets/mercado-pago.png'
+import pagamento from '../../assets/formas-pagamento.png'
 import {BsInstagram, BsWhatsapp} from 'react-icons/bs/index.esm.js'
 import { Link } from 'react-router-dom'
 
@@ -8,23 +8,45 @@ export default function Footer () {
     return(
         <>
         <footer>
-            <img src={logo} alt="1" width="70px" className='logo-img'/>
 
-            <p>
-            44880-000
-            <p>Cafarnaum - BA</p>
-            </p>
+            <div className='card'>
+                <h3>Início</h3>
+                <Link to='/'>Home</Link>
+                <Link to='/sobre'>Sobre Nós</Link>
+                <Link to='https://wa.link/rn6a84' target='_blank'>Contato</Link>
+            </div>
 
-            <img src={cartoes} alt="1" width="230px"/>
+            <div className='card' id='payments'>
+                <h3>Formas de Pagamento</h3>
+                <p>Aceitamos pagamentos por cartão de crédito,
+                    débito e pix.
+                </p>
+                <img src={pagamento} className='img-payments'/>
+            </div>
 
-            <div className='icons-socials'>
-               <Link to='https://www.instagram.com/delplata._/' target='_blank'> <BsInstagram/> </Link>
-               <Link to='https://wa.link/rn6a84' target='_blank'> <BsWhatsapp/> </Link>
+            <div className='card'>
+                <h3>Suporte</h3>
+                <Link to='https://www.instagram.com/delplata._/' target='_blank'>Instagram</Link>
+                <Link to='https://wa.link/rn6a84' target='_blank'>Whatsapp</Link>
+            </div>
+
+            <div className='card'>
+                <h3>Informações</h3>
+                <p>Somos uma empresa especializada em joalheria e trabalhamos com prata 925. 
+                    Um material usado devido à sua durabilidade, resistência e elegância.</p>
             </div>
         </footer>
-        <div className='container-2-footer'>
-            <p>DEL PLATA  &copy; 2023</p>
-            <p>Feito por - <a href='https://yurerafael.dev/' target='_blank'>Yure Rafael</a></p>
+        <div className='icons-socials'>
+            <img src={logo} className='logo-img'/> 
+
+                <p>Del Plata &copy; 2023
+                    <p>Feito por <Link to="https://yurerafael.dev/">Yure Rafael</Link></p>
+                </p>
+
+                <div>
+                <Link to='https://www.instagram.com/delplata._/' target='_blank'> <BsInstagram/> </Link>
+                <Link to='https://wa.link/rn6a84' target='_blank'> <BsWhatsapp/> </Link>
+               </div>
         </div>
         </>
     )
